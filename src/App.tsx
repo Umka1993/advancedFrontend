160ca0f3a14ba5619015ff8b05395eba9f6b110c
Ivan Danilov <ida@inleadmedia.com>
2024-01-17 13:26:55 +0200

    lesson 6-8, set webpack settings.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import * as React from 'react';
-import {Counter} from "./components/Counter";
-import './index.scss';
+import './styles/index.scss';
+import {Link, Route, Routes} from "react-router-dom";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {Suspense, useContext, useState} from "react";
+import "./styles/index.scss";
+import {Theme, ThemeContext} from "./theme/ThemeContext";
+import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 
 const App = () => {
+    const { theme,  toggleTheme } = useTheme();
+
     return (
-        <div className='app'>
-            My App component
-            <Counter/>
+        <div className={classNames('app', {}, [theme, 'class1', 'class2'])}>
+            <button onClick={toggleTheme}>toggle theme</button>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О Сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+               <Routes>
+                   <Route path={'/about'} element={<AboutPageAsync/>}/>
+                   <Route path={'/'} element={<MainPageAsync/>}/>
+               </Routes>
+            </Suspense>
         </div>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import App from "./App";
 import {render} from "react-dom";
 import * as React from 'react'
+import {BrowserRouter} from "react-router-dom";
+import ThemeProvider from "./theme/ThemeProvider";
 
 
 render(
-    <App/>,
+    <BrowserRouter>
+        <ThemeProvider>
+            <App/>
+        </ThemeProvider>
+    </BrowserRouter>,
     document.getElementById('root')
 )
